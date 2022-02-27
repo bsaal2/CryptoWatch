aa361428d365f5927046bc7291f4291a3af96b3b
Bishal Giri <bsaal2@Bishals-MacBook-Pro.local>
2022-02-27 21:31:20 +0545

    [Feature-API] Returned the data from the db of crypto models for get API

--- server/controllers/crypto.js
+++ server/controllers/crypto.js
@@ -1,11 +1,15 @@
+const { crypto } = require('../models');
 const { STATUS_CODE } = require('./constant');
 
 class CryptoController {
-    getAllCrypto = (req, res, next) => {
+    getAllCrypto = async (req, res, next) => {
         try {
+            const cryptoList = await crypto.findAll();
+            
             return res.status(STATUS_CODE.OK).json({
                 status: STATUS_CODE.OK,
-                message: 'Success'
+                message: 'Success',
+                data: cryptoList
             });
         }
 
